a019dde908021d908166159f9fbf611f6271abd0
MagicMotion <MagicMotion2003@proton.me>
2020-09-06 10:08:49 +0000

    Logging

--- Filters.hpp
+++ Filters.hpp
@@ -7,3 +7,7 @@ void applyBinary(Mat &src, Mat &dest, int threshold) {
 
   for (int r = 0; r < src.rows; r++) {
     for (int c = 0; c < src.cols; c++) {
+      if (src.at<uchar>(r, c) > threshold) dest.at<uchar>(r,c) = 255;
+      else dest.at<uchar>(r,c) = 0;
+    }
+  }
