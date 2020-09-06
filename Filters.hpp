48779c991f2f8e4f37cf4b09102b00319ebc4b68
MagicMotion <MagicMotion2003@proton.me>
2020-09-06 05:29:44 +0000

    trunk

--- Filters.hpp
+++ Filters.hpp
@@ -1,2 +1,9 @@
 
 #ifndef FILTERS_H
+#define FILTERS_H
+
+void applyBinary(Mat &src, Mat &dest, int threshold) {
+  dest.create(src.size(), src.type());
+
+  for (int r = 0; r < src.rows; r++) {
+    for (int c = 0; c < src.cols; c++) {
