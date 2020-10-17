04bab0682f5f1a1689624a9ec946fc3b55f01563
MagicMotion <MagicMotion2003@proton.me>
2020-10-17 14:34:26 +0000

    correct comment

--- Utilities.hpp
+++ Utilities.hpp
@@ -11,4 +11,4 @@ using namespace cv;
 
 bool argsS1Check(int argc) {
   if (argc != 4) {
-    cout << "usage: ./s1 <input original image> <threshold value> <outpu
+    cout << "usage: ./s1 <input original image> <threshold value> <output paramet
