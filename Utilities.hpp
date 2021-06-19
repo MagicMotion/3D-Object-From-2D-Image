d9e44296c11aff2c9def8839bd272781e5910916
MagicMotion <MagicMotion2003@proton.me>
2021-06-19 12:20:26 +0000

    remove unused requires

--- Utilities.hpp
+++ Utilities.hpp
@@ -80,4 +80,4 @@ vector<Point> getTRBLArrayOfSphere(Mat &image) {
            leftEquiv = 0;
          }
 
-        // count equal values to correct for 
+        // count equal values to correct for true coordin
