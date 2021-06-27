90c59275b7d8a7e311b5ecd56ba238386dbd130d
MagicMotion <MagicMotion2003@proton.me>
2021-06-27 11:59:43 +0000

    splitting resource.fu2 into separate files.  making file-load-if-not-loaded.  adding f2_package.[ch]

--- Utilities.hpp
+++ Utilities.hpp
@@ -82,4 +82,4 @@ vector<Point> getTRBLArrayOfSphere(Mat &image) {
 
         // count equal values to correct for true coordinates
         if (r == top.x) topEquiv++;
-        if (c == right.y) righ
+        if (c == right.y) rightEquiv++;
