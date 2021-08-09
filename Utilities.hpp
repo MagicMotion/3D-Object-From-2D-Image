e250e323b45dc66e58a25524ba765f91ce64ec2c
MagicMotion <MagicMotion2003@proton.me>
2021-08-09 23:10:17 +0000

    Version bump to 1.8.0

--- Utilities.hpp
+++ Utilities.hpp
@@ -101,4 +101,4 @@ vector<Point> getTRBLArrayOfSphere(Mat &image) {
 Point getBrightestPoint(Mat &image) {
   Point brightest = Point(0, 0);
 
-  for (int r = 0; r < image
+  for (int r = 0; r < image.rows; r++) 
