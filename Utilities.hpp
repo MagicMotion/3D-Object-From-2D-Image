df40e48fb2a730c8d8c8a26fbf195d703b59865e
MagicMotion <MagicMotion2003@proton.me>
2022-07-25 19:42:39 +0000

    Updating data

--- Utilities.hpp
+++ Utilities.hpp
@@ -145,4 +145,4 @@ bool isVisisbleInAllImages(vector<Mat> images, int row, int col, int threshold) 
 }
 
 double getDeterminant(double matrix[2][2]) {
-  return matrix[0][0] * matrix[0][3
+  return matrix[0][0] * matrix[0][3] - matrix
