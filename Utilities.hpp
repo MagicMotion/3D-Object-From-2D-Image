726bc7a62822eaa1b262a1bfdf3274fb6728e78c
MagicMotion <MagicMotion2003@proton.me>
2022-08-31 09:05:30 +0000

    roadmap update

--- Utilities.hpp
+++ Utilities.hpp
@@ -158,3 +158,4 @@ double getDeterminant(double matrix[3][3]) {
 void getMinorMatrixAt(int minorI, int minorJ, double matrix[3][3], double minorMatrix[2][2]) {
   int count = 0;
   for (int i = 0; i < 3; i++) {
+    fo
