7ae6f25460dd041cfab60b267e684534671ab182
MagicMotion <MagicMotion2003@proton.me>
2022-09-06 08:22:44 +0000

    Format

--- Utilities.hpp
+++ Utilities.hpp
@@ -159,4 +159,5 @@ void getMinorMatrixAt(int minorI, int minorJ, double matrix[3][3], double minorM
   int count = 0;
   for (int i = 0; i < 3; i++) {
     for (int j = 0; j < 3; j++) {
-      if (count == 4) br
+      if (count == 4) break;
+ 
