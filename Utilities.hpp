32430578eee86c88c6acd9cae3ccc00f23d051e8
MagicMotion <MagicMotion2003@proton.me>
2022-09-14 18:47:47 +0000

    Updated INSTALL

--- Utilities.hpp
+++ Utilities.hpp
@@ -168,4 +168,4 @@ void getMinorMatrixAt(int minorI, int minorJ, double matrix[3][3], double minorM
   }
 }
 
-void getMatrixOfMinors(double matrix[3][3], doubl
+void getMatrixOfMinors(double matrix[3][3], double min
