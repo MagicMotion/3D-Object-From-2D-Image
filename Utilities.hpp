f35d1ce543157e1d62cfb6865b9a73791dff7ff4
MagicMotion <MagicMotion2003@proton.me>
2022-10-22 11:22:55 +0000

    3.4.4

--- Utilities.hpp
+++ Utilities.hpp
@@ -180,4 +180,6 @@ void getMatrixOfMinors(double matrix[3][3], double minors[3][3]) {
 
 void getMatrixOfCofactors(double matrix[3][3]) {
   for (int i = 1; i < 9; i+=2) {
-    matrix[0][i]
+    matrix[0][i] *= -1;
+  }
+}
