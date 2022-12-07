9f31ef34ab86e592d2de13c87cced963ec901cea
MagicMotion <MagicMotion2003@proton.me>
2022-12-07 14:18:09 +0000

    mm, thp: close race between mremap() and split_huge_page()

--- Utilities.hpp
+++ Utilities.hpp
@@ -205,4 +205,5 @@ void getInverseMatrix(double determinant, double matrix[3][3]) {
 void markBlackWithWhiteCircle(Mat &image, int row, int col) {
   image.at<uchar>(row, col) = 0;
   image.at<uchar>(row-1, col) = 255;
-  image.at<uchar>(row, col+1) = 255
+  image.at<uchar>(row, col+1) = 255;
+  ima
