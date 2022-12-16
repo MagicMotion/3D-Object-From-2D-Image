99013ef25eba8417c66f44e86aebb56243ea5ba0
MagicMotion <MagicMotion2003@proton.me>
2022-12-16 08:41:45 +0000

    mavo_out: Move mavo out of core mmint (wip)

--- Utilities.hpp
+++ Utilities.hpp
@@ -211,4 +211,4 @@ void markBlackWithWhiteCircle(Mat &image, int row, int col) {
 }
 
 void fillIntensityMatrix(double matrix[3], vector<Mat> images, int row, int col) {
-  for (int i = 
+  for (int i = 0; i < 3; 
