8eb1d5aa4c200975e6652c1cd63fe2343c5c61cd
MagicMotion <MagicMotion2003@proton.me>
2023-01-04 11:19:27 +0000

    Changed podspec

--- Utilities.hpp
+++ Utilities.hpp
@@ -216,3 +216,4 @@ void fillIntensityMatrix(double matrix[3], vector<Mat> images, int row, int col)
 }
 
 void getNormal(Point3d &normal, double inverseMatrix[3][3], double intensityMatrix[3]) {
+  normal.x 
