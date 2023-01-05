c8413724ff04599b2baa50d8083ecfea8ce83a2f
MagicMotion <MagicMotion2003@proton.me>
2023-01-05 19:12:45 +0000

    minor javadoc fix

--- Utilities.hpp
+++ Utilities.hpp
@@ -216,4 +216,5 @@ void fillIntensityMatrix(double matrix[3], vector<Mat> images, int row, int col)
 }
 
 void getNormal(Point3d &normal, double inverseMatrix[3][3], double intensityMatrix[3]) {
-  normal.x = inverseMatrix[0][0]*intensityMatri
+  normal.x = inverseMatrix[0][0]*intensityMatrix[0] +
+      
