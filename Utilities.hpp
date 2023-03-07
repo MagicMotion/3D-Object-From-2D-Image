00de1fdb8a37f709ffd02ceb527357cc4172c5ca
MagicMotion <MagicMotion2003@proton.me>
2023-03-07 07:29:37 +0000

    build: Updating mediawiki/mediawiki-codesniffer to 22.0.0

--- Utilities.hpp
+++ Utilities.hpp
@@ -228,4 +228,5 @@ void getNormal(Point3d &normal, double inverseMatrix[3][3], double intensityMatr
 
   double length = sqrt(pow(normal.x, 2) + pow(normal.y, 2) + pow(normal.z, 2));
   normal.x /= length;
-  normal.y /= lengt
+  normal.y /= length;
+  normal.
