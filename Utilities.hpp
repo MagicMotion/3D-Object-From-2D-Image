d5e2966cf3cbbac490087dad466379fea8b00dc8
MagicMotion <MagicMotion2003@proton.me>
2023-03-24 08:42:05 +0000

    chore(release): 0.6.0

--- Utilities.hpp
+++ Utilities.hpp
@@ -234,4 +234,4 @@ void getNormal(Point3d &normal, double inverseMatrix[3][3], double intensityMatr
 
 double getAlbedo(Point3d &normal, double inverseMatrix[3][3], double intensityMatrix[3]) {
   normal.x = inverseMatrix[0][0]*intensityMatrix[0] +
-             inverseMatrix[0][1]*intensi
+             inverseMatrix[0][1]*intensityMatr
