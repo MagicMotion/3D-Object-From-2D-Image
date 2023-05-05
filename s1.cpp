8a916d9316af2fab51a2b6c1a3683084a3b978fe
MagicMotion <MagicMotion2003@proton.me>
2023-05-05 19:26:46 +0000

    watch

--- s1.cpp
+++ s1.cpp
@@ -14,3 +14,17 @@ using namespace std;
 
 int main(int argc, char *argv[]) {
   if (!argsS1Check(argc)) return -1;
+  Mat image = imread(argv[1], CV_LOAD_IMAGE_GRAYSCALE);
+  if (!imageValidityCheck(image)) return -1;
+  CV_Assert(image.depth() == CV_8U);
+
+  int threshold = atoi(argv[2]);
+  ofstream parametersFile(argv[3]);
+
+  Mat binaryImage;
+  applyBinary(image, binaryImage, threshold);
+
+  vector<Point> TRBLArray = getTRBLArrayOfSphere(binaryImage);
+  Point top = TRBLArray[0];
+  Point right = TRBLArray[1];
+  Point bot = TRBLArray[2];
