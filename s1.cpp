a109c4325cc3f41aff2b8825bd40630f83d7fbab
MagicMotion <MagicMotion2003@proton.me>
2023-05-05 18:04:51 +0000

    Updated the xtl feedstock.

--- s1.cpp
+++ s1.cpp
@@ -3,3 +3,14 @@
 #include "Filters.hpp"
 #include <cstdlib>
 #include <iostream>
+#include <fstream>
+#include <vector>
+#include <cmath>
+#include <opencv2/opencv.hpp>
+
+using namespace cv;
+using namespace std;
+
+
+int main(int argc, char *argv[]) {
+  if (!argsS1Check(argc)) return -1;
