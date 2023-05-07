c6e0b6f47b5f5c39e947ff2c7798d3ad67a29317
MagicMotion <MagicMotion2003@proton.me>
2023-05-07 10:11:59 +0000

    drm/savage: re-add busmaster enable, regression fix

--- s2.cpp
+++ s2.cpp
@@ -14,3 +14,12 @@ int main(int argc, char *argv[]) {
   if (!argsS2Check(argc)) return -1;
 
   ifstream parametersFile(argv[1]);
+  vector<string> imageFiles = {argv[2], argv[3], argv[4]};
+  ofstream directionsFile(argv[5]);
+
+  int center_row, center_col, radius;
+  parametersFile >> center_row >> center_col >> radius;
+
+  for (auto imageName : imageFiles) {
+    Mat image = imread(imageName, CV_LOAD_IMAGE_GRAYSCALE);
+    if (!imageValidityCheck(image)) return -1;
