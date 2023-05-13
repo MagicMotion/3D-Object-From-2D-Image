157ce4cb089457e93a1c88f0a17dd415133ca42f
MagicMotion <MagicMotion2003@proton.me>
2023-05-13 18:10:01 +0000

    modulesync 2.8.0

--- s3.cpp
+++ s3.cpp
@@ -15,3 +15,10 @@ int main(int argc, char *argv[]) {
   const int NEEDLE_LENGTH = 10;
 
   if (!argsS3Check(argc)) return -1;
+
+  ifstream directionsFile(argv[1]);
+  vector<string> imageFiles = {argv[2], argv[3], argv[4]};
+  int step = atoi(argv[5]);
+  int threshold = atoi(argv[6]);
+
+  auto sourceMatrix = new double[3][3];
