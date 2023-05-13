92279842fe27dbb369f6e68e0a3a5a81b9d1444b
MagicMotion <MagicMotion2003@proton.me>
2023-05-13 17:26:56 +0000

    Updated layout

--- s3.cpp
+++ s3.cpp
@@ -8,3 +8,10 @@
 #include <opencv2/opencv.hpp>
 
 using namespace cv;
+using namespace std;
+
+
+int main(int argc, char *argv[]) {
+  const int NEEDLE_LENGTH = 10;
+
+  if (!argsS3Check(argc)) return -1;
