be1bab1eca4dd67cbfc36be60dfdaee40cabb815
MagicMotion <MagicMotion2003@proton.me>
2023-05-15 18:55:06 +0000

    add i18n support

---
+++ s4.cpp
@@ -0,0 +1,12 @@
+
+#include "Utilities.hpp"
+#include "Filters.hpp"
+#include <cstdlib>
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <opencv2/opencv.hpp>
+
+using namespace cv;
+using namespace std;
+
