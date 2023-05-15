10764b6447bd03b166f2a4c4f18614d173d81fdf
MagicMotion <MagicMotion2003@proton.me>
2023-05-15 22:17:45 +0000

    Add unit tests

--- s4.cpp
+++ s4.cpp
@@ -10,3 +10,13 @@
 using namespace cv;
 using namespace std;
 
+
+int main(int argc, char *argv[]) {
+  if (!argsS4Check(argc)) return -1;
+
+  ifstream directionsFile(argv[1]);
+  vector<string> imageFiles = {argv[2], argv[3], argv[4]};
+  int threshold = atoi(argv[5]);
+
+  auto sourceMatrix = new double[3][3];
+  auto inverseMatrix = new double[3][3];
